b48893eb4a6d289e32d7c2da6e01b98587b1f9f4
Abhinav <166276971+abhi017z@users.noreply.github.com>
2024-06-12 22:29:30 +0530

    Created list.js
    for the windows alert

---
+++ JS/list.js
@@ -0,0 +1,27 @@
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
+var button4 = document.getElementById("button4");
+var button5 = document.getElementById("button5");
+
+// Add a click event listener to the button
+button1.addEventListener("click", function() {
+
+  window.alert("Appointment at SAGAR HOSPITAL Scheduled!!!");
+});
+button2.addEventListener("click", function() {
+
+  window.alert("Appointment Scheduled!!!");
+});
+button3.addEventListener("click", function() {
+
+  window.alert("Appointment Scheduled!!!");
+});
+button4.addEventListener("click", function() {
+
+  window.alert("Appointment Scheduled!!!");
+});
+button5.addEventListener("click", function() {
+
+  window.alert("Appointment Scheduled!!!");
+});
